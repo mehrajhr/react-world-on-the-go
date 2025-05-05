50285456893e39d89984feaae72b5fb7a9ee0f07
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2025-05-06 00:46:03 +0600

    add visited functionality

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
 const Country = ({country}) => {
     console.log(country);
     const {name} = country;
     const {common} = name;
+    const [visited, setVisited] = useState(false);
+    const handleVisit = () =>{
+        // visited? setVisited(false) : setVisited(true);
+        setVisited(!visited);
+    }
     return (
         <div className='country'>
             <h2>Name : {common}</h2>
             <h3>Capital : {country.capital}</h3>
             <h4>Population : {country.population}</h4>
             <img src={country.flags.png} alt="" />
+            <button className={`${visited && 'country-visited'}`}  onClick={handleVisit}>{
+                visited? 'Visited' : 'Not visited'
+                }</button>
         </div>
     );
 };
